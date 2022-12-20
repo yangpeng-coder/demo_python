47717c1eda77d6ee71aa83441dd7be59371d4a02
h_you <yp.asolu@gmail.com>
2022-12-20 14:10:21 +0000

    個人情報を削除

--- sendIP.py
+++ sendIP.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # Copyright (c) 2022. 楊鵬. All Rights Reserved.
+import os
 import socket
 
 import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 URL = "https://notify-api.line.me/api/notify"
-TOKEN = "mCfw9Yzf8MsXuW2caAPYOkNl7nSewwYppdmV97VRWn6"
 
 
 def sendIP(ip: str):
-    headers = {"Authorization": "Bearer " + TOKEN}
+    headers = {"Authorization": "Bearer " + os.environ["TOKEN"]}
     params = {"message": ip}
     r = requests.post(URL, headers=headers, params=params)
     print(r.text)
